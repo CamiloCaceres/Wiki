ed6cb48e66c3460a0ed72fe42f05fb4214da80c7
Camilo Caceres <camiloac97@gmail.com>
2024-05-20 16:41:49 -0500

    feat: remove Popover from LinkCard

--- components/linkCard.vue
+++ components/linkCard.vue
@@ -1,16 +1,7 @@
 <template>
   <div class="rounded-md p-4 flex items-center justify-between border-2 transition">
     <h4>{{ props.name }}</h4>
-    <UPopover mode="hover">
-      <UButton :to="props.link" target="_blank" size="md" color="primary" square variant="outline" icon="i-heroicons-arrow-top-right-on-square" />
-      <template #panel>
-        <div class="p-2">
-          <p class="text-xs text-gray-400">{{ props.link }}</p>
-        </div>
-      </template>
-    </UPopover>
-
-
+        <UButton :to="props.link" target="_blank" size="md" color="primary" square variant="outline" icon="i-heroicons-arrow-top-right-on-square" />
   </div>
 </template>
 
