e09c172b2a7c9e2cd1b118a2aaee5593c45078db
Camilo Caceres <camiloac97@gmail.com>
2024-05-22 16:42:11 -0500

    Feat. create credit-transfer page

--- pages/knowledge-base/index.vue
+++ pages/knowledge-base/index.vue
@@ -1,15 +1,26 @@
 <template>
   <h1 class="font-bold text-2xl mb-4">Knowledge Base</h1>
-  <UCard>
-    <h2 class="font-semibold text-xl">Visa information:</h2>
+  <UCard class="mb-4" v-for="page in pages">
+    <h2 class="font-semibold text-xl">{{ page.name }}</h2>
     <template #footer>
-      <UButton>
-        <NuxtLink to="/knowledge-base/visas"> Go to Visas </NuxtLink>
-      </UButton>
+      <div class="flex justify-end">
+        <UButton>
+          <NuxtLink :to="page.to"> Go to Visas </NuxtLink>
+        </UButton>
+      </div>
     </template>
   </UCard>
 </template>
 
 <script setup>
-
+const pages = [
+  {
+    name: "Visa Information",
+    to: "/knowledge-base/visas",
+  },
+  {
+    name: "Credit Transfer",
+    to: "/knowledge-base/credit-transfer",
+  },
+];
 </script>
