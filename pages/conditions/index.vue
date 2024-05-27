fd699a4cf05f1dbc01853053604194314ca2f3c1
Camilo Caceres <camiloac97@gmail.com>
2024-05-27 13:32:00 -0500

    Feat: add Delete, add DoN, add BN buttons to Conditions

--- pages/conditions/index.vue
+++ pages/conditions/index.vue
@@ -1,38 +1,92 @@
 <template>
   <div>
     <h1 class="font-bold text-xl my-4">Conditions:</h1>
-    <UPopover class="h-full" v-model:open="copied" :popper="{ placement: 'top' }">
+    <UPopover
+      class="h-full"
+      v-model:open="copied"
+      :popper="{ placement: 'top' }"
+    >
       <div class="flex w-full mb-6">
-      <div class="whitespace-pre-line grow text-gray-600 rounded p-2 h-40 overflow-auto">{{ outputText }}</div>
+        <div
+          class="whitespace-pre-line grow text-gray-600 rounded p-2 h-40 overflow-auto"
+        >
+          {{ outputText }}
+        </div>
 
-      <UButton @click="copyToClipboard"
-        :icon="copied ? 'i-heroicons-clipboard-document-check' : 'i-heroicons-clipboard-document'" size="lg"
-        color="primary" square variant="solid" />
-
-
-    </div>
+        <UButton
+          @click="copyToClipboard"
+          :icon="
+            copied
+              ? 'i-heroicons-clipboard-document-check'
+              : 'i-heroicons-clipboard-document'
+          "
+          size="lg"
+          color="primary"
+          square
+          variant="solid"
+        />
+      </div>
       <template #panel>
         <div class="p-2 bg-lime-200">
-          <p class=" text-lime-700">Copied!</p>
+          <p class="text-lime-700">Copied!</p>
         </div>
       </template>
     </UPopover>
-    
   </div>
   <h2 class="font-bold text-lg mb-4">Select conditions:</h2>
   <div class="grid grid-cols-4 gap-4 mb-4">
-    <div v-for="condition in conditions.filter((condition) => !condition.isNursing)">
-      <Condition v-bind="condition" @toggle-select="toggleSelect(condition.id)" />
+    <div
+      v-for="condition in conditions.filter(
+        (condition) => !condition.isNursing
+      )"
+    >
+      <Condition
+        v-bind="condition"
+        @toggle-select="toggleSelect(condition.id)"
+      />
     </div>
   </div>
-
 
   <h2 class="font-bold text-lg mb-4">Nursing Conditions:</h2>
   <div class="grid grid-cols-4 gap-4">
-    <div v-for="condition in conditions.filter((condition) => condition.isNursing)">
-      <Condition v-bind="condition" @toggle-select="toggleSelect(condition.id)" />
+    <div
+      v-for="condition in conditions.filter((condition) => condition.isNursing)"
+    >
+      <Condition
+        v-bind="condition"
+        @toggle-select="toggleSelect(condition.id)"
+      />
     </div>
   </div>
+  <h2 class="font-bold text-lg my-4">Actions</h2>
+
+<div class="flex items-center space-x-4">
+  <UButton
+    @click="deselectAllConditions"
+    class="mb-4"
+    color="primary"
+    variant="outline"
+    icon="i-heroicons-trash"
+  >
+    Deselect All Conditions
+  </UButton>
+  <UButton
+    @click="addBNConditions"
+    class="mb-4"
+    color="primary"
+    variant="solid"
+  >
+    Add BN Conditions
+  </UButton>
+  <UButton
+    @click="addDoNConditions"
+    class="mb-4"
+    color="primary"
+    variant="solid"
+  >
+    Add DoN Conditions
+  </UButton>
+</div>
 </template>
 
 <script setup lang="ts">
@@ -42,22 +96,60 @@ import { useClipboard } from "@vueuse/core";
 
 const { copy, copied } = useClipboard();
 
-const conditions = reactive(conditionsFile)
+const conditions = reactive(conditionsFile);
 /* 
 const nursingConditions = computed(() => conditions.value.filter((condition) => condition.isNursing));
 const nonNursingConditions = computed(() => conditions.value.filter((condition) => !condition.isNursing));
  */
-const selectedConditions = computed(() => conditions.filter((condition) => condition.isSelected));
-const conditionsList = computed(() => selectedConditions.value.map((condition) => `- ${condition.text}`).join("\n"));
-const outputText = computed(() => `This offer is conditional upon:\n ${conditionsList.value}`);
-
+const selectedConditions = computed(() =>
+  conditions.filter((condition) => condition.isSelected)
+);
+const conditionsList = computed(() =>
+  selectedConditions.value.map((condition) => `- ${condition.text}`).join("\n")
+);
+const outputText = computed(
+  () => `This offer is conditional upon:\n ${conditionsList.value}`
+);
 
 const toggleSelect = (conditionId: number) => {
   const condition = conditions.find((c) => c.id === conditionId);
   if (condition) {
     condition.isSelected = !condition.isSelected;
   }
-}
+};
+const deselectAllConditions = () => {
+  conditions.forEach((condition) => {
+    condition.isSelected = false;
+  });
+};
+
+const addBNConditions = () => {
+  const bnConditionNames = [
+    "NMBA",
+    "capacity",
+    "APRHA",
+    "Complete DoN",
+    "Credits",
+  ];
+  conditions.forEach((condition) => {
+    if (bnConditionNames.includes(condition.name)) {
+      condition.isSelected = true;
+    }
+  });
+};
+
+const addDoNConditions = () => {
+  const DoNConditionNames = [
+    "NMBA",
+    "capacity",
+    "LLN",
+  ];
+  conditions.forEach((condition) => {
+    if (DoNConditionNames.includes(condition.name)) {
+      condition.isSelected = true;
+    }
+  });
+};
 
 const copyToClipboard = () => {
   copy(outputText.value);
