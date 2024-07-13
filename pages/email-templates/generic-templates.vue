18b4c78e79822e8c8393e5a1f2ec7a0f157c891f
Camilo Caceres <camiloac97@gmail.com>
2024-07-13 15:24:53 -0500

    fix: correct title

--- pages/email-templates/generic-templates.vue
+++ pages/email-templates/generic-templates.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="font-bold text-2xl mb-4">Credit Transfer Templates</h1>
+    <h1 class="font-bold text-2xl mb-4">Generic Templates</h1>
   
     <div class="flex flex-col space-y-6">
       <div v-for="(template, index) in genericTemplates" :key="index">
