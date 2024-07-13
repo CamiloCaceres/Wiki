b82826efc7d042c84b7e005875585f36e186c1aa
Camilo Caceres <camiloac97@gmail.com>
2024-07-13 16:53:30 -0500

    feat: add TOC to each content page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,23 @@
 <template>
+<nav class="table-of-contents">
+      <ul>
+        <li v-for="item in links?.children" :key="item.id">
+          <UButton variant="link":to="`/process/${links?.id}#${item.id}`">- {{ item.text }}</UButton>
+        </li>
+      </ul>
+    </nav>
+
   <main class="prose">
     <ContentDoc />
   </main>
 </template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+const { data: page } = await useAsyncData(route.path, () => queryContent(route.path).findOne())
+
+const links = page.value?.body?.toc?.links[0]
+
+</script>
+
