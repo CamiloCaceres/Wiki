6a774c3c0174be5eb5463d61cd5fda4989f62fda
Camilo Caceres <camiloac97@gmail.com>
2024-07-15 18:04:38 -0500

    ui: create grid to place TOC

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,23 +1,27 @@
 <template>
-<nav class="table-of-contents">
+  <div class="grid md:grid-cols-8">
+    <main class="prose md:col-span-6 md: col-start-2 ">
+      <ContentDoc />
+    </main>
+
+    <nav class="col-span-2">
       <ul>
         <li v-for="item in links?.children" :key="item.id">
-          <UButton variant="link":to="`/process/${links?.id}#${item.id}`">- {{ item.text }}</UButton>
+          <UButton variant="link" :to="`/process/${links?.id}#${item.id}`"
+            >- {{ item.text }}</UButton
+          >
         </li>
       </ul>
     </nav>
-
-  <main class="prose">
-    <ContentDoc />
-  </main>
+  </div>
 </template>
 
 <script setup lang="ts">
-const route = useRoute()
+const route = useRoute();
 
-const { data: page } = await useAsyncData(route.path, () => queryContent(route.path).findOne())
+const { data: page } = await useAsyncData(route.path, () =>
+  queryContent(route.path).findOne()
+);
 
-const links = page.value?.body?.toc?.links[0]
-
+const links = page.value?.body?.toc?.links[0];
 </script>
-
