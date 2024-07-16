c5b64c7c2cc0632cf1e4d3433548b2c43b3c6ba4
Camilo Caceres <camiloac97@gmail.com>
2024-07-16 16:34:35 -0500

    feat: add main navigation

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="grid md:grid-cols-8">
-    <main class="prose md:col-span-6 md: col-start-2 ">
+  <div class="grid md:grid-cols-8 gap-4">
+    <div class="col-span-2">
+      <UVerticalNavigation :links="links" />
+      
+    </div>
+
+    <main class="prose md:col-span-6">
       <ContentDoc />
     </main>
 
     <nav class="col-span-2">
       <ul>
-        <li v-for="item in links?.children" :key="item.id">
-          <UButton variant="link" :to="`/process/${links?.id}#${item.id}`"
+        <li v-for="item in localLinks?.children" :key="item.id">
+          <UButton variant="link" :to="`/process/${localLinks?.id}#${item.id}`"
             >- {{ item.text }}</UButton
           >
         </li>
@@ -19,9 +24,30 @@
 <script setup lang="ts">
 const route = useRoute();
 
+const { data: navigation } = await useAsyncData("navigation", () =>
+  fetchContentNavigation()
+);
+
+function transformNavigation(data: any) {
+  if (!data || data.length === 0 || !data[0].children) {
+    return []
+  }
+
+  const transformItem = (item: any) => ({
+    label: item.title,
+    to: item._path
+  })
+
+  return data[0].children.map(transformItem)
+}
+
+
+const links = computed(() =>
+  navigation.value ? transformNavigation(navigation.value) : []
+);
 const { data: page } = await useAsyncData(route.path, () =>
   queryContent(route.path).findOne()
 );
 
-const links = page.value?.body?.toc?.links[0];
+const localLinks = page.value?.body?.toc?.links[0];
 </script>
