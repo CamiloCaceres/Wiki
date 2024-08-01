2c08183c09dff1fc6b1b45c95d577b9a11d95051
Camilo Caceres <camiloac97@gmail.com>
2024-08-01 18:08:42 -0500

    feat: add NuxtImage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "nuxt-tiptap-editor", '@pinia/nuxt', "@nuxt/content"],
+  modules: [
+    "@nuxt/ui",
+    "nuxt-tiptap-editor",
+    '@pinia/nuxt',
+    "@nuxt/content",
+    "@nuxt/image"
+  ],
 })
