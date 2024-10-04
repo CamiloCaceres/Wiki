f0073c1a518556ce57522b079b5fc60ac8b6a083
Camilo Caceres <camiloac97@gmail.com>
2024-10-04 09:21:04 -0500

    feat: add pocketbase composables

--- composables/usePocketbase.ts
+++ composables/usePocketbase.ts
@@ -1,57 +1,69 @@
-import { ref } from 'vue'
-import PocketBase from 'pocketbase'
+import { ref } from "vue";
+import PocketBase from "pocketbase";
 
 export function usePocketbase() {
-  const config = useRuntimeConfig()
-  const pocketbaseUrl = config.public.pocketbaseUrl as string
+  const config = useRuntimeConfig();
+  const pocketbaseUrl = config.public.pocketbaseUrl as string;
 
   if (!pocketbaseUrl) {
-    console.error('POCKETBASE_URL is not defined in the environment variables')
+    console.error("POCKETBASE_URL is not defined in the environment variables");
   }
 
-  const pb = new PocketBase(pocketbaseUrl)
+  const pb = new PocketBase(pocketbaseUrl);
 
   //Auth
-  const currentUser = ref(pb.authStore.model)
+  const currentUser = ref(pb.authStore.model);
 
   pb.authStore.onChange((auth) => {
-    console.log('authStore changed', auth)
-    currentUser.value = pb.authStore.model
-  })
-
+    console.log("authStore changed", auth);
+    currentUser.value = pb.authStore.model;
+  });
 
   const logout = () => {
-    pb.authStore.clear()
-  }
+    pb.authStore.clear();
+  };
 
-// Studylink
+  // Studylink
 
-const getAllStudylink = async () => {
-  try {
-    const records = await pb.collection('studylink').getFullList();
-    return records;
-  } catch (error) {
-    console.error('Error fetching all studylink:', error);
-    throw error;
-  }
-}
+  const getAllStudylink = async () => {
+    try {
+      const records = await pb.collection("studylink").getFullList();
+      return records;
+    } catch (error) {
+      console.error("Error fetching all studylink:", error);
+      throw error;
+    }
+  };
 
-const getStudylinkBySlug = async (slug: string) => {
-  try {
-    const record = await pb.collection('studylink').getFirstListItem(`slug="${slug}"`);
-    return record;
-  } catch (error) {
-    console.error('Error fetching studylink:', error);
-    throw error;
-  }
-}
-
-const getImageUrl = (item: any) => {
-  if (item && item.image) {
-    return pb.files.getUrl(item, item.image, { thumb: "800x0" });
-  }
-  return "";
-};
+  const getStudylinkBySlug = async (slug: string) => {
+    try {
+      const record = await pb
+        .collection("studylink")
+        .getFirstListItem(`slug="${slug}"`);
+      return record;
+    } catch (error) {
+      console.error("Error fetching studylink:", error);
+      throw error;
+    }
+  };
+  const searchStudylinkByKeyword = async (query: string) => {
+    try {
+      const records = await pb.collection("studylink").getList(1, 50, {
+        fields: "name,slug,description,process,keywords",
+        filter: `(keywords ~ "${query}") || (name ~ "${query}") || (description ~ "${query}")`,
+      });
+      return records;
+    } catch (error) {
+      console.error("Error searching studylink:", error);
+      throw error;
+    }
+  };
+  const getImageUrl = (item: any) => {
+    if (item && item.image) {
+      return pb.files.getUrl(item, item.image, { thumb: "800x0" });
+    }
+    return "";
+  };
 
   return {
     pb,
@@ -59,6 +71,7 @@ const getImageUrl = (item: any) => {
     logout,
     getAllStudylink,
     getStudylinkBySlug,
-    getImageUrl
-  }
+    getImageUrl,
+    searchStudylinkByKeyword,
+  };
 }
