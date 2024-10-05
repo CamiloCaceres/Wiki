0edf85193699ef6f27f1f23bdeb72caa32b25c10
camilo caceres <camiloac97@gmail.com>
2024-10-05 14:41:20 -0500

    refactor: call pb from useQuery

---
+++ pages/procedures/[slug].vue
@@ -0,0 +1,51 @@
+<template>
+    <div v-if="isPending">
+      <div class="space-y-4">
+        <USkeleton class="h-8 w-3/4" />
+        <USkeleton class="h-4 w-1/2" />
+        <div class="space-y-2">
+          <USkeleton class="h-4 w-full" v-for="i in 5" :key="i" />
+        </div>
+      </div>
+    </div>
+    <div v-else-if="isError">
+      <h1 class="text-2xl font-bold">Error: {{ error?.message }}</h1>
+    </div>
+    <div v-else-if="data">
+      <h1 class="text-3xl font-extrabold">{{ data.name }}</h1>
+      <p class="text-gray-500 mt-2 mb-4">{{ data.description }}</p>
+      <div v-html="data.content" class="prose md:prose-lg dark:prose-invert p-2"></div>
+      <p class="text-sm text-gray-500 my-2 italic">Last updated {{ new Date(data.updated).toLocaleDateString() }}</p>
+    </div>
+    <div v-else>
+      <h1 class="text-2xl font-bold">Procedure not found, either reload the page or contact the admin</h1>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { useRoute } from 'vue-router';
+  import { usePocketbase } from '@/composables/usePocketbase';
+  import { useQuery } from '@tanstack/vue-query';
+  
+  const route = useRoute();
+  const { getProcedureBySlug } = usePocketbase();
+  
+  const slug = route.params.slug as string;
+  
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ['getProcedureBySlug', slug],
+    queryFn: () => getProcedureBySlug(slug),
+  });
+  
+  watch(() => data.value, (newData) => {
+    if (newData) {
+      useHead({
+        title: newData.name
+      });
+    }
+  }, { immediate: true });
+  
+  definePageMeta({
+    layout: 'studylink'
+  });
+  </script>
--- pages/procedures/index.vue
+++ pages/procedures/index.vue
@@ -2,10 +2,25 @@
   <div>
     <h1 class="text-3xl my-4 font-bold">Procedures</h1>
   </div>
-  <div v-if="procedures && procedures.length > 0">
-    <SearchProcess :processes="procedures" />
+  <div v-if="isPending">
     <div
-      v-for="procedure in procedures"
+      v-for="i in 5"
+      :key="i"
+      class="p-4 flex justify-between items-center shadow-md rounded-md mt-4"
+    >
+      <div class="flex flex-col gap-2 w-full">
+        <USkeleton class="h-6 w-3/4" />
+        <USkeleton class="h-4 w-full" />
+      </div>
+    </div>
+  </div>
+  <div v-else-if="isError">
+    <h1>Error: {{ error?.message }}</h1>
+  </div>
+  <div v-else>
+    <SearchProcess :processes="data" />
+    <div
+      v-for="procedure in data"
       :key="procedure.id"
       @click="router.push(`/procedures/${procedure.slug}`)"
       class="p-4 flex justify-between items-center cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800 border border-gray-200 dark:border-gray-800 rounded-md mt-4"
@@ -20,37 +35,16 @@
       </div>
     </div>
   </div>
-  <div v-else-if="loading">
-    <div
-      v-for="i in 5"
-      :key="i"
-      class="p-4 flex justify-between items-center shadow-md rounded-md mt-4"
-    >
-      <div class="flex flex-col gap-2 w-full">
-        <USkeleton class="h-6 w-3/4" />
-        <USkeleton class="h-4 w-full" />
-      </div>
-    </div>
-  </div>
-  <div v-else>
-    <h1>No procedures found</h1>
-  </div>
+
 </template>
 
 <script setup lang="ts">
-const { getAllProcedures } = usePocketbase();
+import { usePocketbase } from "@/composables/usePocketbase";
+import { useRouter } from "vue-router";
+import { useQuery } from '@tanstack/vue-query'
+
 const router = useRouter();
+const { getAllProcedures } = usePocketbase();
 
-const procedures = ref();
-const loading = ref(true);
-
-onMounted(async () => {
-  try {
-    procedures.value = await getAllProcedures();
-  } catch (error) {
-    console.error("Error fetching procedures:", error);
-  } finally {
-    loading.value = false;
-  }
-});
+const { isPending, isError, data, error } = useQuery({ queryKey: ['getAllProcedures'], queryFn: getAllProcedures })
 </script>
