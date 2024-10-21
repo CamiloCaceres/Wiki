d52a3bfd3c62473e1e3640ffefd5e53b1ee47e46
Camilo Caceres <camiloac97@gmail.com>
2024-10-21 11:05:07 -0500

    feat: add cloudfare analytics

--- app.vue
+++ app.vue
@@ -6,3 +6,14 @@
     <UNotifications />
   </div>
 </template>
+<script setup>
+useHead({
+  script: [
+    {
+      src: 'https://static.cloudflareinsights.com/beacon.min.js',
+      defer: true,
+      'data-cf-beacon': '{"token": "a9d61fd8ac3b46cfb58ee1654cafb4d8"}',
+    }
+  ]
+})
+</script>
