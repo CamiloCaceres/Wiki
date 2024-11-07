af8320992f64e78041df607162e002e0485b48c4
Camilo Caceres <camiloac97@gmail.com>
2024-11-06 21:22:24 -0500

    feat: add notifications component

---
+++ components/Notification.vue
@@ -0,0 +1,38 @@
+<template>
+  <UButton variant="link" @click="openModal = true"> View </UButton>
+  <UModal v-model="openModal" :fullscreen="message ? true : false">
+    <UCard>
+      <template v-if="message" #header>
+        <div class="flex justify-between items-center">
+          <h1>{{ title }}</h1>
+          <UButton
+            icon="i-heroicons-x-mark"
+            color="red"
+            @click="openModal = false"
+          >
+          </UButton>
+        </div>
+      </template>
+      <div v-if="message" v-html="message"></div>
+      <div v-else>
+        <h1>{{ title }}</h1>
+      </div>
+      <template #footer>
+        <div class="flex justify-end">
+          <UButton color="red" variant="outline" @click="openModal = false"
+            >Close</UButton
+          >
+        </div>
+      </template>
+    </UCard>
+  </UModal>
+</template>
+
+<script setup lang="ts">
+const openModal = ref(false);
+
+const props = defineProps<{
+  title: string;
+  message: string;
+}>();
+</script>
